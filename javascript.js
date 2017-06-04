2f1eb228516a8d7b804419c706aa3d5a0c5b367c
erinlevine <erinclevine@gmail.com>
2017-06-04 15:56:43 -0400

    Added a boatload of CSS

--- javascript.js
+++ javascript.js
@@ -17,11 +17,11 @@
 //------------------------------------------
 
 //Array of foodstuffs
-var foodstuffs = ["Pizza", "Pasta", "Tacos", "Doughnuts", "Cupcakes", "Waffles", "Fried Chicken", "Burrito", "Eggs", "Cheese", "Pineapple", "Sushi", "Cereal"];
+var foodStuffs = ["Pizza", "Pasta", "Tacos", "Doughnuts", "Cupcakes", "Waffles", "Fried Chicken", "Burrito", "Eggs", "Cheese", "Pineapple", "Sushi", "Cereal"];
 
 //displayFoodGifs is the function to display the food GIFs from GIPHY
 function displayFoodGifs() {
-	var food = $(this).attr("food-name");
+	var food = $(this).attr("data-name");
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + food + "&limit=10&rating&api_key=dc6zaTOxFJmzC";
 
 //Create AJAX call for the specific foodstuffs being clicked
@@ -35,11 +35,47 @@ function displayFoodGifs() {
 	   var newDiv = $("<div>");
 
 	   //Logging the rating for the gif
-	   console.log(response.rating);
+	   console.log(response.data.rating);
 
 	   //Appending the Rating and Gif to HTML
 	   $("#foodGifs").empty();
-	   $("#foodGifs").html("Rating: " + response.rating);
-	   $("#foodGifs").append(response)
+	   $("#foodGifs").html("Rating: " + response.data.rating);
+	   $("#foodGifs").append(response) //I HAVE NO IDEA WHAT THIS URL WILL BE!
+	});
+}
+
+//Function for displaying food gifs
+function buttonsAppear(){
+
+	$("#foodButtonsView").empty();
+	//Looping through our foodStuffs array
+	for(var i = 0; i < foodStuffs.length; i++) {
+		//Creating buttons for each food in the foodStuffs array
+		var a = $("<button>");
+		//Adding a class of "food" to the button
+		a.addClass("food");
+		//Adding a data attribute
+		a.attr("data-name", foodStuffs[i]);
+		//The text of the button
+		a.text(foodStuffs[i]);
+		//Adding the button to the div
+		$("#foodButtonsView").append(a);
+		//I'm making the buttons pretty below:
+		$("button").css("background-color", "red");
+		$("button").css("font-family", "'Roboto', sans-serif");
+		$("button").css("border-radius", "20px");
+		$("button").css("padding", "7px");
+		$("button").css("opacity", ".8");
 	}
 }
+
+	//Function that handles event where the add food button is clicked. LINE 91
+	$("#add-food").on("click", function(event){
+		event.preventDefault();
+		var food = $("#food-input").val().trim();
+		foodStuffs.push(food);
+		buttonsAppear();
+	});
+	$(document).on("click", ".food", displayFoodGifs);
+
+	buttonsAppear();
